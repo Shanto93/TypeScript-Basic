a7529ad7656d5dfb28aaba053138d3e953510509
Shanto93 <shantoislam7363@gmail.com>
2024-06-23 10:36:06 +0600

    function,object,array,type alias,rest parameter are added

---
+++ src/typeAlias.ts
@@ -0,0 +1,30 @@
+//Type Alias
+type CrushType = {
+  name: string;
+  age?: number;
+  profession: string;
+};
+
+const crush01: CrushType = {
+  name: "Sanjida Tanjir",
+  age: 24,
+  profession: "Web Developer",
+};
+
+const crush02: CrushType = {
+  name: "no crush",
+  profession: "no profession",
+};
+
+//type alias
+type CalculateType = (x: number, y: number) => number;
+const calculate = (
+  number1: number,
+  number2: number,
+  operation: CalculateType
+) => {
+  return operation(number1, number2);
+};
+
+calculate(10, 20, (x, y) => x + y);
+calculate(10, 20, (x, y) => x - y);
