2247bf14f06ddcae27b93cfc235f7ada4ec112a9
Shanto93 <shantoislam7363@gmail.com>
2024-06-23 14:35:43 +0600

    null,unknown and never type is added

---
+++ src/null-unknown-never.ts
@@ -0,0 +1,33 @@
+//Null
+const searching = (value: string | null) => {
+  if (value === null) {
+    //console.log("Nothing to search");
+  } else {
+    //onsole.log("Searching");
+  }
+};
+
+searching("Shanto");
+searching(null);
+
+//Unknown
+const convert = (value: unknown) => {
+  if (typeof value === "number") {
+    const converted = (value * 1000) / 3600;
+   // console.log(`Speed is ${converted} m^h`);
+  } else if (typeof value === "string") {
+    const [num, unit] = value.split(" ");
+    const converted = (parseFloat(value) * 1000) / 3600;
+    // console.log(`Speed is ${converted} m^h`);
+  } else {
+    // console.log("There is wrong type");
+  }
+};
+convert(10);
+convert("10 km^h");
+
+//Never
+function throwError(message: string): never {
+  throw new Error(message);
+}
+throwError("Error has been occured");
