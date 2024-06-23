13419053aef0b18ab5a1196c0a7f43a51194b5dc
Shanto93 <shantoislam7363@gmail.com>
2024-06-23 11:01:39 +0600

    Union, Intersection and Enum type is added

---
+++ src/union-intersection.ts
@@ -0,0 +1,28 @@
+type NoobDeveloper = {
+  name: string;
+};
+
+type JuniorDeveloper = NoobDeveloper | {    //Union
+  experience: number;
+};
+
+type SeniorDeveloper = JuniorDeveloper & {     //Intersection
+  level: string;
+};
+
+const JuniorDeveloper = {
+  name: "Shanto",
+  experience: 2,
+};
+
+enum Level {                         //Enum
+  low = "Low",
+  mid = "Medium",
+  high = "High",
+}
+
+const developer: SeniorDeveloper = {
+  name: "shanto",
+  experience: 2,
+  level: Level.high,
+};
