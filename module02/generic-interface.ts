9d246f285ac1890c8cabff51c99a0129382d0869
Shanto93 <shantoislam7363@gmail.com>
2024-06-28 21:10:09 +0600

    Some complex generic with interface practice

--- module02/generic-interface.ts
+++ module02/generic-interface.ts
@@ -1,24 +1,57 @@
-interface IMarriedDetails<T> {
-  name: string;
-  husband: T;
+// interface IMarriedDetails<T> {
+//   name: string;
+//   husband: T;
+// }
+
+// const Shanto: IMarriedDetails<string> = {
+//   name: "Sanjida Tanjir",
+//   husband: "Shanto",
+// };
+
+// type MarriedInfoTuple<T, S> = [T, S];
+// type WifeDetailes<W> = {
+//   wife: W;
+// };
+
+// const relation: MarriedInfoTuple<IMarriedDetails<string>, WifeDetailes<string>> = [
+//   {
+//     name: "Sanjida Tanjir",
+//     husband: "Shanto",
+//   },
+//   {
+//     wife: "Shanto Islam",
+//   },
+// ];
+
+
+interface ICrush<T,S>{
+    name: string;
+    husband: T;
+    wife: S;
 }
 
-const Shanto: IMarriedDetails<string> = {
-  name: "Sanjida Tanjir",
-  husband: "Shanto",
-};
+type HusbandDetails<F=null> = {
+    name: string;
+    age: number;
+    family?: F;
+}
+type WifeDetails = {
+    name: string;
+    age: number;
+}
 
-type MarriedInfoTuple<T, S> = [T, S];
-type WifeDetailes<W> = {
-  wife: W;
-};
-
-const relation: MarriedInfoTuple<IMarriedDetails<string>, WifeDetailes<string>> = [
-  {
+const crush1: ICrush<HusbandDetails<string>, WifeDetails> = 
+{
     name: "Sanjida Tanjir",
-    husband: "Shanto",
-  },
-  {
-    wife: "Shanto Islam",
-  },
-];
+    husband:{
+        name: "Shanto",
+        age: 24,
+        family: "Parents, Sister",
+    },
+    wife:
+    {
+        name:"Sanjida Tanjir",
+        age: 25,
+    }
+
+}
