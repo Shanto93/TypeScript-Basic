f97fc33958258c936b6b2ddf449244cda1d69181
Shanto93 <shantoislam7363@gmail.com>
2024-06-30 12:12:12 +0600

    One error example is added

--- module02/mapped-type.ts
+++ module02/mapped-type.ts
@@ -1,16 +1,18 @@
-// const arrayNumbers = [1,2,3,4,5];
-// const arrayString = arrayNumbers.map((numbers) => numbers.toString());
-// console.log(arrayString);
+// // const arrayNumbers = [1,2,3,4,5];
+// // const arrayString = arrayNumbers.map((numbers) => numbers.toString());
+// // console.log(arrayString);
 
-type Volume = {
-  height: number;
-  width: number;
-  depth: number;
-};
+// type Volume = {
+//   height: number;
+//   width: number;
+//   depth: number;
+// };
 
-type Change<T> = {
-  [key in keyof T]: T[key];
-};
+// type Change<T> = {
+//   readonly [key in keyof T]: T[key];   //Can't change value if we use readonly keyword
+// };
 
-const area1: Change<Volume> = { height: 1, width: 2, depth: 3 };
-console.log(area1);
+// const area1: Change<Volume> = { height: 1, width: 2, depth: 3 };
+// console.log(area1);
+
+// // area1.depth = 10;    //It is showing error as we used readonly keyword so we can't change the value
