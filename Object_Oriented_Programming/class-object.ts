b4a7eb21ca40a15cd5765194d57b4b79cce68bdf
Shanto93 <shantoislam7363@gmail.com>
2024-07-01 21:58:21 +0600

    Example of Parameter Property is added

--- Object_Oriented_Programming/class-object.ts
+++ Object_Oriented_Programming/class-object.ts
@@ -1,19 +1,25 @@
-class Animal{
-    name: string;
-    species: string;
-    sounds: string;
+class Animal {
+  // name: string;
+  // species: string;
+  // sounds: string;
 
-    constructor(name: string, species: string, sounds: string)
-    {
-        this.name = name;
-        this.species = species;
-        this.sounds = sounds;
-    }
-    makesound()
-    {
-        console.log(`${this.name} sounds like ${this.sounds}`);
-    }
+  //Using Parameter Property
+  constructor(
+    public name: string,
+    public species: string,
+    public sounds: string
+  ) {
+    // this.name = name;
+    // this.species = species;
+    // this.sounds = sounds;
+  }
+  makesound() {
+    console.log(`${this.name} sounds like ${this.sounds}`);
+  }
 }
 
 const dog = new Animal("Fido", "Dog", "Ghew Ghew");
+const cat = new Animal("Tommy", "Cat", "Meaw Meaw");
+
 dog.makesound();
+cat.makesound();
