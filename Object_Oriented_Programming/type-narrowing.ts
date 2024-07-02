4359ad6d6e5c654f0ffdc33b18519ff9d7d4d313
Shanto93 <shantoislam7363@gmail.com>
2024-07-02 12:13:08 +0600

    in guard is added

--- Object_Oriented_Programming/type-narrowing.ts
+++ Object_Oriented_Programming/type-narrowing.ts
@@ -1,15 +1,38 @@
+// //Keyof Guard
 
-//Keyof Guard
-function add(
-  param1: number | string,
-  param2: number | string
-): number | string {
-  if (param1 == "number" && param2 == "number") {
-    return param1 + param2;
+// type Alphanumeric = number | string;
+// function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
+//   if (param1 == "number" && param2 == "number") {
+//     return param1 + param2;
+//   } else {
+//     return param1.toString() + param2.toString();
+//   }
+// }
+
+// console.log(add("1", "2"));
+// console.log(add(3, 4));
+
+//in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: `admin`;
+};
+
+const getUser = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    return `This is admin user`;
   } else {
-    return param1.toString() + param2.toString();
+    return `This is normal user`;
   }
-}
+};
 
-console.log(add("1", "2"));
-console.log(add(3, 4));
+const normal1: NormalUser = { name: "Shanta Islam" };
+const admin1: AdminUser = { name: "Sanjida Tanjir", role: "admin" };
+
+console.log(getUser(normal1));
+console.log(getUser(admin1));
