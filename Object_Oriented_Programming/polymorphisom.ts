fc927f122250aacc7739965b58572b5657c0a7e8
Shanto93 <shantoislam7363@gmail.com>
2024-07-02 19:13:07 +0600

    Polymorphisom is added

---
+++ Object_Oriented_Programming/polymorphisom.ts
@@ -0,0 +1,41 @@
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  radius: number;
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rectangle extends Shape {
+  height: number;
+  width: number;
+  constructor(height: number, width: number) {
+    super();
+    this.height = height;
+    this.width = width;
+  }
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+function getAreaOfShape(shape: Shape) {
+  console.log(shape.getArea());
+}
+
+const area1 = new Shape();
+const area2 = new Circle(5);
+const area3 = new Rectangle(5, 5);
+
+getAreaOfShape(area1);
+getAreaOfShape(area2);
+getAreaOfShape(area3);
