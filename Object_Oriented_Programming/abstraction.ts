258ccc08246948752177390cd6a3228d033ee2d2
Shanto93 <shantoislam7363@gmail.com>
2024-07-02 20:14:27 +0600

    Abstruction using Interface is done

---
+++ Object_Oriented_Programming/abstraction.ts
@@ -0,0 +1,32 @@
+//Abstraction using Interface
+
+interface IVehicle {
+  startEngine(): void;
+  endEngine(): void;
+  test(): void;
+}
+
+class Vehicle implements IVehicle {
+  constructor(
+    public name: string,
+    public speed: number,
+    public model: number
+  ) {}
+
+  startEngine(): void {
+    console.log("Starting Engine");
+  }
+  endEngine(): void {
+    console.log("End Engine");
+  }
+  test(): void {
+    console.log("Test Engine");
+  }
+  extraTest(): void {
+    console.log("Extra Test");
+  }
+}
+
+const vehicle1 = new Vehicle("Toyota", 180, 2010);
+
+//Abstraction Using Abstract Class
